732aa728ce562c7312e7df4fb009d03cb24bb2b5
MJ20027 <mi02code@gmail.com>
2023-07-06 10:51:29 +0530

    second commit

--- src/app/MyComponents/table-details/table-details.component.ts
+++ src/app/MyComponents/table-details/table-details.component.ts
@@ -1,4 +1,48 @@
 import { Component } from '@angular/core';
+import datainfo from 'src/assets/data.json';
+
+interface Newdata {
+  momId: string;
+  title: string;
+  venue: string;
+  meetingDate: string;
+  chairPerson: string;
+  mode: string;
+  keywords: string;
+
+  momAttendeesResponse:{
+    attendeesId: string;
+    name: string;
+    designation:string;
+    organization:string;
+    email: string;
+    type: string;
+  }[];
+
+  momActionResponse:{
+    title: string;
+    momId: string | null;
+    actionId: string;
+    decissionPoint: string;
+    actionPoint: string;
+    status: string;
+  }[];
+
+  editedBy:string;
+  editTime:string;
+  remarks:string;
+  status:string;
+  linkedMOM:string;
+  pdfFile:string;
+  momEditHistoryResponse:{
+    momEditId: string;
+    editedBy: string;
+    editTime: string;
+    remarks: string;
+    status: string;
+  }[];
+
+}
 
 @Component({
   selector: 'app-table-details',
@@ -7,4 +51,5 @@ import { Component } from '@angular/core';
 })
 export class TableDetailsComponent {
 
+ OrigData:Newdata[]=datainfo;
 }
